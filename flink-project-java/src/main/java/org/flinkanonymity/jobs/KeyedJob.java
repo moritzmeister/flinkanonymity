7af5985447e1221e0724cdf1f8d5b42ea863f230
Philip Claesson <philipcl@kth.se>
2018-03-11 18:02:45 +0100

    Evaluation

--- flink-project-java/src/main/java/org/flinkanonymity/jobs/KeyedJob.java
+++ flink-project-java/src/main/java/org/flinkanonymity/jobs/KeyedJob.java
@@ -26,78 +26,92 @@ public class KeyedJob {
     static HashMap<String, Bucket> hashMap;
 
     /* Anonymity and Diversity Parameters */
-    private static int k = 10;
-    private static int l = 4;
-    private static int parallelism = 4;
+    private static int k = 50;
+    private static int l = 10;
+    private static int parallelism = 10;
 
     /* Stream Parameters */
     private static int uniqueAdults = 300;
-    private static int streamLength = 20000;
+    private static int streamLength = 50000;
 
 
     /* -- Hierarchy Levels -- */
-    private static int ageHierarchy = 1;
+    private static int ageHierarchy = 2;
     private static int sexHierarchy = 0;
     private static int raceHierarchy = 0;
     private static int educHierarchy = 2;
-    private static int marstHierarchy = 0;
-    private static int countryHierarchy = 1;
+    private static int marstHierarchy = 1;
+    private static int countryHierarchy = 0;
+
+    private static int[] kvals = {40, 40, 40, 40, 40, 40, 40, 40, 40, 40};
+    private static int[] lvals = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    private static int[] pvals = {10, 20, 30, 40};
+
 
 
 
     public static void main(String[] args) throws Exception {
-        ParameterTool params = ParameterTool.fromArgs(args);
-        // final String filePath = params.getRequired("input");
+        for(int i = 0; i < 10; i++){
 
-        // Set data file path
-        String dataFilePath = "../sample-data/arx_adult/adult_sensitive.csv";
+            k = kvals[i];
+            l = lvals[i];
+            //parallelism = pvals[i];
 
-        // Set Hierarchy files paths.
-        String sex_hierarchy = "../sample-data/arx_adult/adult_hierarchy_sex.csv";
-        String age_hierarchy = "../sample-data/arx_adult/adult_hierarchy_age.csv";
-        String race_hierarchy = "../sample-data/arx_adult/adult_hierarchy_race.csv";
-        String marst_hierarchy = "../sample-data/arx_adult/adult_hierarchy_marital-status.csv";
-        String educ_hierarchy = "../sample-data/arx_adult/adult_hierarchy_education.csv";
-        String country_hierarchy = "../sample-data/arx_adult/adult_hierarchy_native-country.csv";
-        // String workclass_hierarchy = "../sample-data/arx_adult/adult_hierarchy_workclass.csv";
-        // String occ_hierarchy = "../sample-data/arx_adult/adult_hierarchy_occupation.csv";
-        // String salary_hierarchy = "../sample-data/arx_adult/adult_hierarchy_salary-class.csv";
 
-        // Initialize generalizations
-        Generalization age = new Generalization("age", age_hierarchy, ageHierarchy);
-        Generalization sex = new Generalization("sex", sex_hierarchy, sexHierarchy);
-        Generalization race = new Generalization("race", race_hierarchy, raceHierarchy);
-        Generalization educ = new Generalization("educ", educ_hierarchy,educHierarchy);
-        Generalization marst = new Generalization("marst", marst_hierarchy,marstHierarchy);
-        Generalization country = new Generalization("country", country_hierarchy,countryHierarchy);
-        //Generalization workclass = new Generalization("workclass", workclass_hierarchy,1);
+            ParameterTool params = ParameterTool.fromArgs(args);
+            // final String filePath = params.getRequired("input");
 
-        // Initialize QuasiIdentifier
-        QID = new QuasiIdentifier(age, sex, race, educ, marst, country);
+            // Set data file path
+            String dataFilePath = "../sample-data/arx_adult/adult_sensitive.csv";
 
-        // Setting up Environment
-        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        env.setParallelism(parallelism);
-        env.setStreamTimeCharacteristic(TimeCharacteristic.IngestionTime);
-        DataStream<AdultData> data = env.addSource(new AdultDataSource(dataFilePath, uniqueAdults, streamLength, QID));
+            // Set Hierarchy files paths.
+            String sex_hierarchy = "../sample-data/arx_adult/adult_hierarchy_sex.csv";
+            String age_hierarchy = "../sample-data/arx_adult/adult_hierarchy_age.csv";
+            String race_hierarchy = "../sample-data/arx_adult/adult_hierarchy_race.csv";
+            String marst_hierarchy = "../sample-data/arx_adult/adult_hierarchy_marital-status.csv";
+            String educ_hierarchy = "../sample-data/arx_adult/adult_hierarchy_education.csv";
+            String country_hierarchy = "../sample-data/arx_adult/adult_hierarchy_native-country.csv";
+            // String workclass_hierarchy = "../sample-data/arx_adult/adult_hierarchy_workclass.csv";
+            // String occ_hierarchy = "../sample-data/arx_adult/adult_hierarchy_occupation.csv";
+            // String salary_hierarchy = "../sample-data/arx_adult/adult_hierarchy_salary-class.csv";
 
-        // Generalize Quasi Identifiers
-        DataStream<AdultData> genData = data.map(new Generalize());
+            // Initialize generalizations
+            Generalization age = new Generalization("age", age_hierarchy, ageHierarchy);
+            Generalization sex = new Generalization("sex", sex_hierarchy, sexHierarchy);
+            Generalization race = new Generalization("race", race_hierarchy, raceHierarchy);
+            Generalization educ = new Generalization("educ", educ_hierarchy,educHierarchy);
+            Generalization marst = new Generalization("marst", marst_hierarchy,marstHierarchy);
+            Generalization country = new Generalization("country", country_hierarchy,countryHierarchy);
+            //Generalization workclass = new Generalization("workclass", workclass_hierarchy,1);
 
-        // Write Flink ingestion time into the AdultData objects
-        DataStream<AdultData> tsGenData = genData
-                .keyBy(new QidKey())
-                .process(new ProcessTimestamp());
+            // Initialize QuasiIdentifier
+            QID = new QuasiIdentifier(age, educ, marst);
+            //QID = new QuasiIdentifier(age, educ, marst);
 
-        DataStream<AdultData> output = tsGenData
-                .keyBy(new QidKey())
-                .window(GlobalWindows.create())
-                .trigger(lDiversityTrigger.of(k, l))
-                .process(new Release());
+            // Setting up Environment
+            StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+            env.setParallelism(parallelism);
+            env.setStreamTimeCharacteristic(TimeCharacteristic.IngestionTime);
+            DataStream<AdultData> data = env.addSource(new AdultDataSource(dataFilePath, uniqueAdults, streamLength, QID));
 
-        output.print();
-        String filename = "output-n_" + streamLength + "k_" + k + "l_" + l + "p_" + parallelism + ".csv";
-        output.writeAsText("../output/" + filename, org.apache.flink.core.fs.FileSystem.WriteMode.OVERWRITE).setParallelism(1); // setParalellism(1) yields ONE output file.
-        env.execute();
+            // Generalize Quasi Identifiers
+            DataStream<AdultData> genData = data.map(new Generalize());
+
+            // Write Flink ingestion time into the AdultData objects
+            DataStream<AdultData> tsGenData = genData
+                    .keyBy(new QidKey())
+                    .process(new ProcessTimestamp());
+
+            DataStream<AdultData> output = tsGenData
+                    .keyBy(new QidKey())
+                    .window(GlobalWindows.create())
+                    .trigger(lDiversityTrigger.of(k, l))
+                    .process(new Release());
+
+            output.print();
+            String filename = "output-n_" + streamLength + "k_" + k + "l_" + l + "p_" + parallelism + ".csv";
+            output.writeAsText("../output/" + filename, org.apache.flink.core.fs.FileSystem.WriteMode.OVERWRITE).setParallelism(1); // setParalellism(1) yields ONE output file.
+            env.execute();
+        }
     }
 }
